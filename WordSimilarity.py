2a32547c97cd08164d391f18dc44fa884f201b7d
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-22 13:15:46 -0400

    adds additional functionality to the methods.

--- WordSimilarity.py
+++ WordSimilarity.py
@@ -1,12 +1,14 @@
-
+import nltk
+from nltk.corpus import stopwords
 
 class WordSimilarity():
-
+    stop_words = None
     def __init__(self) -> None:
         """
         Constructor that sets up all the required variables.
         """
-        pass
+        nltk.download("stopwords")
+        self.stop_words = set(stopwords.words("english"))
     
     def nomarlizer(self, document) -> list():
         """
@@ -16,6 +18,23 @@ class WordSimilarity():
         2. Tokenization.
         """
         try:
-            pass
+            document = document.lower()
+            tokens = document.split()
+            for token in tokens:
+                if token.isalpha() and token not in self.stop_words:
+                    continue
+                else:
+                    tokens.remove(token)
+
+            return tokens
         except Exception as e:
             print("[ERR] The following error occured while trying to normalize text: "+str(e))
+
+    def compare_texts_w2v(file_one, file_two, k=10) -> None:
+        """
+        Find the top K words, and then find the cosine similarity.
+        """
+        try:
+            pass
+        except Exception as e:
+            print("[ERR] The following error occured while trying to compare texts using w2v: "+str(e))
