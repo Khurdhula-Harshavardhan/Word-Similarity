3156b62ca58cb2422ae3738881304dcf6130ef94
Harsha <khurdhulaharshavardhan@gmail.com>
2023-03-22 12:58:54 -0400

    defines method prototype for normalizer.

---
+++ WordSimilarity.py
@@ -0,0 +1,21 @@
+
+
+class WordSimilarity():
+
+    def __init__(self) -> None:
+        """
+        Constructor that sets up all the required variables.
+        """
+        pass
+    
+    def nomarlizer(self, document) -> list():
+        """
+        Returns a list of all words, that have been normalized.
+        Normalization here is performed as:
+        1. Case Conversion into Lower for case consistency.
+        2. Tokenization.
+        """
+        try:
+            pass
+        except Exception as e:
+            print("[ERR] The following error occured while trying to normalize text: "+str(e))
